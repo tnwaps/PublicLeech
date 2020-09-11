fd42011eaf7a41090f67feb5b47d6351b02a1e64
tnwaps <65656829+tnwaps@users.noreply.github.com>
2020-09-12 01:14:27 +0530

    Update dinmamoc.py

--- tobrot/dinmamoc.py
+++ tobrot/dinmamoc.py
@@ -20,7 +20,7 @@ from tobrot.get_cfg import get_config
 class Commandi:
     LEECH = get_config(
         "COMMANDI_LEECH",
-        "leech"
+        "leech@torlecbot"
     )
     PURGE = get_config(
         "COMMANDI_PURGE",
@@ -28,7 +28,7 @@ class Commandi:
     )
     YTDL = get_config(
         "COMMANDI_YTDL",
-        "ytdl"
+        "ytdl@torlecbot"
     )
     STATUS = get_config(
         "COMMANDI_STATUS",
@@ -36,7 +36,7 @@ class Commandi:
     )
     CANCEL = get_config(
         "COMMANDI_CANCEL",
-        "cancel"
+        "cancel@torlecbot"
     )
     EXEC = get_config(
         "COMMANDI_EXEC",
@@ -48,23 +48,23 @@ class Commandi:
     )
     RENAME = get_config(
         "COMMANDI_RENAME",
-        "rename"
+        "rename@torlecbot"
     )
     UPLOAD = get_config(
         "COMMANDI_UPLOAD",
-        "upload"
+        "upload@torlecbot"
     )
     HELP = get_config(
         "COMMANDI_HELP",
-        "help"
+        "help@torlecbot"
     )
     SAVETHUMBNAIL = get_config(
         "COMMANDI_SAVETHUMBNAIL",
-        "savethumbnail"
+        "savethumb@torlecbot"
     )
     CLEARTHUMBNAIL = get_config(
         "COMMANDI_CLEARTHUMBNAIL",
-        "clearthumbnail"
+        "clearthumb@torlecbot"
     )
     GET_RCLONE_CONF_URI = get_config(
         "COMMANDI_GET_RCLONE_CONF_URI",
